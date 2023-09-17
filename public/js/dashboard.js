bc0ace715cacbc67df68ce1090b2f761496bc036
Adrien Ferron <62189215+N0r3f@users.noreply.github.com>
2023-09-17 21:25:25 +0200

    Delete public directory

--- public/js/dashboard.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('add-image-form').addEventListener('submit', function(event) {
-    // Empêcher le formulaire d'être soumis normalement
-    event.preventDefault();
-  
-    // Code pour ajouter une image
-  });
